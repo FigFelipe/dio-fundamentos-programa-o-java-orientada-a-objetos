30627d561c3580ee8f63ac29175ee027ce44c084
Felipe Figueiredo <felipe_figueiredo@outlook.com.br>
2024-07-03 11:03:31 -0300

    Teste

--- src/com/mastersoft/clinical/service/ExameService.java
+++ src/com/mastersoft/clinical/service/ExameService.java
@@ -1,6 +1,6 @@
 package com.mastersoft.clinical.service;
 
-// Importando a classe Exame de outro pacote
+// Importando a classe Exame de outro pacotes
 import com.mastersoft.clinical.model.Exame;
 
 public class ExameService {
